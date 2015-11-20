51c606018933de42c30ef588c8485e57338d9a0d
Bryce Ito <bito001@ucr.edu>
2015-11-20 15:42:42 -0800

    updated parse.cc with new commands

--- parse.cc
+++ parse.cc
@@ -9,6 +9,9 @@ using namespace std;
 vector<string> parsing (char *line) {
   vector <char*> result;
   unsigned size = 0;
+  
+  //if a '#' is found, ignore rest of commmand
+  //find size of char array
   for (; line[size] != '\0'; size++) {
     if (line[size] == '#') {
       line[size] = '\0';
@@ -18,9 +21,16 @@ vector<string> parsing (char *line) {
 
 //  cout << line << endl;
 
+  //list of symbols to parse
   char semi[] = ";";
   char amp[] = "&&";
   char bars[] = "||";
+  char sqrf[] = "[";
+  char sqrr[] = "]";
+  char parenf[] = "(";
+  char parenr[] = ")";
+  char test[] = "test";
+
   char command[size + 1];
   unsigned count = 0;
   for (unsigned i = 0; line[i] != '\0'; i++) {
@@ -54,6 +64,68 @@ vector<string> parsing (char *line) {
       command[0] = '\0';
       i+=2;
       count = 0;
+    }
+
+    if (line[i] == '[') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(sqrf);
+      command[0] = '\0';
+      count = 0;
+      i++;
+    }
+
+    if (line[i] == ']') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(sqrr);
+      command[0] = '\0';
+      count = 0;
+      i++;
+    }
+
+    if (line[i] == '(') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(parenf);
+      command[0] = '\0';
+      count = 0;
+      i++;
+    }
+   
+    if (line[i] == ')') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(parenr);
+      command[0] = '\0';
+      count = 0;
+      i++;
+    }
+
+    if (line[i] == ' ' && line[i+1] == 't' && line[i+2] == 'e' &&
+        line[i+3] == 's' && line[i+4] == 't' && line[i+5] == ' ') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(test);
+      command[0] = '\0';
+      count = 0;
+      i+=5;
+    }
+
+    if (i == 0 && line[i] == 't' && line[i+1] == 'e' &&
+        line[i+2] == 's' && line[i+3] == 't' && line[i+4] == ' ') {
+      char* copy = new char[count+1];
+      strcpy (copy, command);
+      result.push_back(copy);
+      result.push_back(test);
+      command[0] = '\0';
+      count = 0;
+      i+=4;
     }
 
     command[count] = line[i];
